f260abcdaebed93d62387d358d3558a87106d713
gigaflw <a_gigantic_flower@yeah.net>
2018-01-22 21:27:00 +0800

    basic popup page

---
+++ popup.js
@@ -0,0 +1,81 @@
+function setColor(theme) {
+  chrome.storage.local.set({
+    'colorful-github': theme
+  }, () => {
+    chrome.tabs.executeScript({
+      file: 'colorful.js'
+    });
+  });
+}
+
+function getSavedColor(callback) {
+  chrome.storage.sync.get('selected', (item) => {
+    callback(chrome.runtime.lastError ? null : item);
+  });
+}
+
+function initStorage() {
+  let colors = ['#111', '#555', '#888', '#bbb', '#eee'] // from more to less
+  let colorsThreshold = [10, 8, 5, 3, 0];
+  let colorObj = Array.from(colors, (_, ind) => ({
+    color: colors[ind],
+    threshold: colorsThreshold[ind]
+  }))
+
+  let themes = [{
+    name: 'foo1',
+    colors: colors,
+    thresholds: colorsThreshold
+  }, {
+    name: 'foo2',
+    colors: colors,
+    thresholds: colorsThreshold
+  }, ]
+
+  chrome.storage.sync.set({
+    'colorful-github-all': themes,
+    'colorful-github-selected': '',
+  });
+
+  return themes
+}
+
+function initDropdown(themes) {
+  if (!themes) return;
+  let opts = document.createDocumentFragment();
+  for (let theme of themes) {
+    let opt = document.createElement('option');
+    opt.value = theme.name;
+    opt.appendChild(document.createTextNode(theme.name));
+    opts.appendChild(opt);
+  }
+  document.getElementById('color-select').appendChild(opts);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  let dropdown = document.getElementById('color-select');
+  let themes = null;
+
+  chrome.storage.sync.get('colorful-github-all', (obj) => {
+    if (!obj['colorful-github-all']) {
+      themes = initStorage();
+    } else {
+      themes = obj['colorful-github-all'];
+    }
+    initDropdown(themes);
+
+    chrome.storage.sync.get('colorful-github-selected', (obj) => {
+      if (!chrome.runtime.lastError) {
+        dropdown.value = obj['colorful-github-selected'];
+      }
+    });
+
+    dropdown.addEventListener('change', () => {
+      setColor(themes.find(t => t.name == dropdown.value));
+      chrome.storage.sync.set({
+        'colorful-github-selected': dropdown.value,
+      });
+    });
+  });
+
+});
