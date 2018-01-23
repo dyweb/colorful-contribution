e26e67060b5ccd1eef3d2d2a388991c0f1c7d4fb
gigaflw <a_gigantic_flower@yeah.net>
2018-01-23 11:51:26 +0800

    beautify popup

--- popup.js
+++ popup.js
@@ -2,14 +2,16 @@
  * @Author: gigaflower
  * @Date:   2017-11-19 13:55:57
  * @Last Modified by:   gigaflw
- * @Last Modified time: 2018-01-22 22:15:48
+ * @Last Modified time: 2018-01-23 14:49:16
  */
 
 /*
  * Save the selected `theme` into `chrome.storage.local` so that
  * content script `colorful.js` can find it.
  */
-function setTheme(theme) {
+function sendTheme(theme) {
+  if (!theme) return
+
   if (!theme.thresholds) {
     theme.thresholds = window.CGC_CONFIG.default_thresholds
   }
@@ -37,21 +39,39 @@ function initStorage() {
 /*
  * Add options to popup pages according to `themes`
  */
-function initDropdown(themes) {
+function initButtons(themes) {
   if (!themes) return
-  let opts = document.createDocumentFragment()
+
+  let fragment = document.createDocumentFragment()
   for (let theme of themes) {
-    let opt = document.createElement('option')
-    opt.value = theme.name
-    opt.appendChild(document.createTextNode(theme.name))
-    opts.appendChild(opt)
+    let themeBlock = document.createElement('div'),
+      themeNameBlock = document.createElement('div'),
+      themeColorBlock = document.createElement('div')
+
+    themeNameBlock.classList.add('theme-name')
+    themeNameBlock.appendChild(document.createTextNode(theme.name))
+
+    themeColorBlock.classList.add('theme-colors')
+    for (let color of theme.colors) {
+      let colorBlock = document.createElement('div')
+      colorBlock.classList.add('color-block')
+      colorBlock.style['background-color'] = color
+      themeColorBlock.appendChild(colorBlock)
+    }
+
+    themeBlock.classList.add('theme-block')
+    themeBlock.dataset.name = theme.name
+    themeBlock.appendChild(themeNameBlock)
+    themeBlock.appendChild(themeColorBlock)
+
+    fragment.appendChild(themeBlock)
   }
-  document.getElementById('color-select').appendChild(opts)
+  document.getElementById('color-select').appendChild(fragment)
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  let dropdown = document.getElementById('color-select')
+  let colorSelect = document.getElementById('color-select')
   let themes = null
 
   chrome.storage.sync.get('colorful-github-all', (obj) => {
@@ -62,19 +82,35 @@ document.addEventListener('DOMContentLoaded', () => {
       themes = obj['colorful-github-all']
     }
 
-    initDropdown(themes)
+    initButtons(themes)
+
+    function setTheme(themeName) {
+      sendTheme(themes.find(t => t.name == themeName))
+      chrome.storage.sync.set({ 'colorful-github-selected': themeName })
+      colorSelect.dataset.selected = themeName
+      Array.prototype.forEach.call(colorSelect.querySelectorAll('.theme-block'), blk => {
+        if (blk.dataset.name === themeName) {
+          blk.classList.add('selected')
+        } else {
+          blk.classList.remove('selected')
+        }
+      })
+    }
 
     chrome.storage.sync.get('colorful-github-selected', (obj) => {
       if (!chrome.runtime.lastError) {
-        dropdown.value = obj['colorful-github-selected']
+        setTheme(obj['colorful-github-selected'])
       }
     })
 
-    dropdown.addEventListener('change', () => {
-      setTheme(themes.find(t => t.name == dropdown.value))
-      chrome.storage.sync.set({
-        'colorful-github-selected': dropdown.value,
-      })
+    colorSelect.addEventListener('click', (event) => {
+      let elem = event.target
+
+      while (!elem.classList.contains('theme-block')) {
+        elem = elem.parentNode
+      }
+      // every .theme-block should have a data-name field
+      setTheme(elem.dataset.name)
     })
   })
 
